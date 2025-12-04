ebe174608342c455a7dad67f539a40abe6c3394e
chris-tiee <czb00@mail.aub.edu>
2025-12-04 12:03:21 +0100

    only added the ID and checked old functionality

--- wiferion_charger/launch/wiferion.launch.py
+++ wiferion_charger/launch/wiferion.launch.py
@@ -24,8 +24,8 @@ def generate_launch_description():
         'canbus_dev', default_value='vcan1', description='CAN bus device'
     )
 
-    arg_id = DeclareLaunchArgument(
-        'charger_ID',
+    arg_charger_id = DeclareLaunchArgument(
+        'charger_id',
         default_value='0',
         description='Charger ID'
     )
@@ -34,7 +34,7 @@ def generate_launch_description():
     namespace = LaunchConfiguration('namespace')
     launch_gateway = LaunchConfiguration('launch_gateway')
     canbus_dev = LaunchConfiguration('canbus_dev')
-    charger_id = LaunchConfiguration('charger_ID')
+    charger_id = LaunchConfiguration('charger_id')
 
     # Clearpath ROS2 Socketcan Interface
     pkg_clearpath_ros2_socketcan_interface = FindPackageShare('clearpath_ros2_socketcan_interface')
@@ -68,14 +68,14 @@ def generate_launch_description():
         name='wiferion_node',
         executable='wiferion_node',
         package='wiferion_charger',
-        parameters=[{'canbus_dev': canbus_dev}, {'charger_ID': id}],
+        parameters=[{'canbus_dev': canbus_dev}, {'charger_id': charger_id}],
         namespace=namespace,
         output='screen',
     )
 
     ld = LaunchDescription()
     ld.add_action(arg_canbus_dev)
-    ld.add_action(arg_id)
+    ld.add_action(arg_charger_id)
     ld.add_action(arg_launch_gateway)
     ld.add_action(arg_namespace)
     ld.add_action(launch_receiver)
